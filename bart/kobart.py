830d8ec89ab26efd65076d586cc984949992d8b2
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-05-14 17:21:13 +0900

    Create kobart.py

---
+++ bart/kobart.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import PreTrainedTokenizerFast
+from transformers import BartForConditionalGeneration
+
+tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
+model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
+
+file_path = 'diary.txt'
+
+with open(file_path, 'r', encoding='utf8') as file:
+    text = file.read()
+
+raw_input_ids = tokenizer.encode(text)
+input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
+
+summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
+summary_text = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+
+file_path = 'summarize.txt'
+
+with open(file_path,'w',encoding='utf8') as file:
+    file.write(summary_text)
+
+print(summary_text)
