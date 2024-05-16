91945fb626867904273528a2d9ba139d58061f98
YuSeungTae <tmdxo1102@gmail.com>
2024-05-16 21:28:27 +0900

    [Feat] - [요약모델 모듈화]

--- bart/kobart.py
+++ bart/kobart.py
@@ -5,20 +5,23 @@ from transformers import BartForConditionalGeneration
 tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
 model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
 
-file_path = 'diary.txt'
+file_path = '/home/test01/yykc/bart/diary.txt'
 
 with open(file_path, 'r', encoding='utf8') as file:
     text = file.read()
 
-raw_input_ids = tokenizer.encode(text)
-input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
 
-summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
-summary_text = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
+def summarization(text):
+    raw_input_ids = tokenizer.encode(text)
+    input_ids = [tokenizer.bos_token_id] + raw_input_ids + [tokenizer.eos_token_id]
 
-file_path = 'summarize.txt'
+    summary_ids = model.generate(torch.tensor([input_ids]),  num_beams=4,  max_length=512,  eos_token_id=1)
+    summary_text = tokenizer.decode(summary_ids.squeeze().tolist(), skip_special_tokens=True)
 
-with open(file_path,'w',encoding='utf8') as file:
-    file.write(summary_text)
+    # file_path = '/home/test01/yykc/bart/summarize.txt'
 
-print(summary_text)
+    # with open(file_path,'w',encoding='utf8') as file:
+    #     file.write(summary_text)
+
+    print(f"summary_text: {summary_text}")
+    return summary_text
