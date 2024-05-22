479c6b19984a07985a35bd1f7686c72219e6005a
YuSeungTae <tmdxo1102@gmail.com>
2024-05-22 17:19:34 +0900

    [Feat] - [tts 클로바 보이스로 변경]

--- tts/tts.py
+++ tts/tts.py
@@ -1,3 +1,10 @@
+import os
+import sys
+import urllib.request
+import json
+import os, sys; sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+from username import username
+
 import pygame
 
 import os, sys; sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
@@ -24,18 +31,36 @@ def play_wav_file(file_path):
         pygame.quit()
 
 
-from gtts import gTTS
 
 
 
 def PrintComment(text):
+    
+    with open(f"/home/{username}/yykc/tts/api_key.json", 'r') as file:
+        config = json.load(file)
+
     path = f'/home/{username}/yykc/tts/comment/recent_comment.wav'
-    tts = gTTS(
-        text=text,
-        lang='ko', slow=False
-    )
-    tts.save(path)
+    client_id = config["client_id"]
+    client_secret = config["client_secret"]
+    encText = urllib.parse.quote(text)
+    data = "speaker=ndain&volume=0&speed=0&pitch=0&format=mp3&text=" + encText
+    url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
+    request = urllib.request.Request(url)
+    request.add_header("X-NCP-APIGW-API-KEY-ID",client_id)
+    request.add_header("X-NCP-APIGW-API-KEY",client_secret)
+    response = urllib.request.urlopen(request, data=data.encode('utf-8'))
+    rescode = response.getcode()
+
+
+    if(rescode==200):
+        response_body = response.read()
+        with open(path, 'wb') as f:
+            f.write(response_body)
+    else:
+        print("Error Code:" + rescode)
+
     play_wav_file(path)
+
 
 
 
